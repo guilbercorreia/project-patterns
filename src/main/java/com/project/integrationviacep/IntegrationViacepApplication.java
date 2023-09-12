df3ea9d9387a53f3ee4565a0cd0fd7220e07a62f
Guilber Correia <guilber.correia.gc@gmail.com>
2023-09-12 16:35:18 -0300

    Primeiro commit

---
+++ src/main/java/com/project/integrationviacep/IntegrationViacepApplication.java
@@ -0,0 +1,13 @@
+package com.project.integrationviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationViacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationViacepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/integrationviacep/IntegrationViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.integrationviacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
